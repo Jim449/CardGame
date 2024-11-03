e64f837173e61e2799c7010a289d44dfc3826926
Jim Ström <alexius.strom@gmail.com>
2024-11-03 22:15:21 +0100

    Improved card drawer

--- card_drawer.py
+++ card_drawer.py
@@ -3,99 +3,112 @@ from PIL import Image, ImageDraw, ImageFont
 
 class CardDrawer():
     def __init__(self):
-        # 26 pt
-        # 24 is good!
         self.header_font = ImageFont.truetype("Fonts/MANASPC.TTF", 24)
-        # 13 pt
-        # 12 is good!
         self.ability_font = ImageFont.truetype("Fonts/MANASPC.TTF", 12)
-        # 16pt
-        # 16 is good!
         self.attributes_font = ImageFont.truetype(
             "Fonts/MERCUTIONBPSMALLCAPS-V2AA.TTF", 16)
-        # 18pt
-        # 16 is good!
         self.details_font = ImageFont.truetype("Fonts/RETGANON.TTF", 16)
-        # 10pt
-        # 12 is good! It actually has some light grey in it
-        # I'm not sure if I can get rid of it
-        self.flavour_font = ImageFont.truetype("Fonts/ZELDADXT.TTF", 12)
+        self.flavour_font = ImageFont.truetype("Fonts/ZELDADXT.TTF", 11)
 
         self.color = (0, 0, 0)
 
-        # Centered
         self.header_y = 23
-        self.long_header_y = 8
-        # 12 was a bit far to the right. Same for all letters?
-        self.ability_pos = (11, 128)
-        # Right
-        self.ability_value_pos = (219, 128)
-        self.ability_attr_pos = (12, 140)
-        self.details_pos = (12, 159)
-        self.details_spacing = 5
+        self.long_header_y = 9
+        self.header_spacing = 9
 
-        self.flavor_pos = (12, 301)
-        self.flavor_spacing = 11
+        self.ability_x = 11
+        self.ability_y = 128
+        self.line_y = 141
+        self.line_start = 12
+        self.line_end = 219
+        self.value_x = 221
+        self.ability_attr_x = 12
+        self.ability_attr_y = 140
+        self.details_x = 12
+        self.details_y = 159
+        self.details_spacing = 5
+        self.details_height = 17
+        self.ability_height = 40
+        # 32 + 2*21 = 74
+        # 32 + 21 = 53
+
+        # 40 + 17 = 57
+        # 40 + 2*17 = 74
+
+        self.flavour_x = 12
+        self.flavour_y = 308
+        self.flavour_spacing = 2
 
         self.attributes_pos = (12, 315)
-        self.elements_pos = (11, 335)
-        # Right
-        self.health_pos = (219, 335)
+        self.elements_y = 335
 
     def draw_header(self, image: ImageDraw, header: str):
         length = image.textlength(header, font=self.header_font)
         x = 116 - length // 2
         image.text((x, self.header_y), header, fill=self.color,
-                   font=self.header_font, align="center")
+                   font=self.header_font)
 
     def draw_long_header(self, image: ImageDraw, header: str):
-        length = image.textlength(header, font=self.header_font)
-        x = 116 - length // 2
-        image.multiline_text((x, self.long_header_y),
-                             header, fill=self.color,
-                             font=self.header_font, align="center")
+        box = image.multiline_textbbox((0, 0), header, font=self.header_font,
+                                       spacing=self.header_spacing)
+        # Box[2] = right side, equals length in this case
+        x = 116 - box[2] // 2
+        image.multiline_text((x, self.long_header_y), header, fill=self.color,
+                             font=self.header_font, spacing=self.header_spacing)
 
-    def draw_card_text(self, image: ImageDraw, ability: str, ability_value: str, ability_attr: str,
-                       ability_details: str, attributes: str, elements: str, health: str):
-        image.text(self.ability_pos, ability,
-                   fill=self.color, font=self.ability_font)
-        image.text(self.ability_value_pos, ability_value, fill=self.color,
-                   font=self.ability_font, align="right")
-        image.text(self.ability_attr_pos, ability_attr,
-                   fill=self.color, font=self.attributes_font)
-        image.multiline_text(self.details_pos, ability_details, fill=self.color,
-                             font=self.details_font, spacing=self.details_spacing)
+    def draw_footer(self, image: ImageDraw, attributes: str, elements: str, health: str):
+        length = image.textlength(health, font=self.ability_font)
+        x = self.value_x - length
         image.text(self.attributes_pos, attributes,
                    fill=self.color, font=self.attributes_font)
-        image.text(self.elements_pos, elements,
+        image.text((self.ability_x, self.elements_y), elements,
                    fill=self.color, font=self.ability_font)
-        image.text(self.health_pos, health, fill=self.color,
-                   font=self.ability_font, align="right")
+        image.text((x, self.elements_y), health, fill=self.color,
+                   font=self.ability_font)
 
-    def add_ability(self, image: ImageDraw, space: int, ability: str, ability_value: str, ability_attr: str,
-                    ability_details: str):
-        pass
+    def draw_ability(self, image: ImageDraw, ability: str, ability_attr: str,
+                     ability_details: str, ability_value: str = "", spacing: int = 0):
+        if spacing > 0:
+            pushdown = self.ability_height + spacing * self.details_height
+        else:
+            pushdown = 0
 
-    def add_passive(self, image: ImageDraw, space: int, ability: str, ability_value: str, ability_details: str):
-        pass
+        length = image.textlength(ability_value, font=self.ability_font)
+        x = self.value_x - length
 
-    def add_flavour(self, image: ImageDraw, spaces: int, text: str):
-        position = (
-            self.flavor_pos[0], self.flavor_pos[1] - self.flavor_spacing * spaces)
-        image.multiline_text(
-            position, text, fill=self.color, font=self.flavour_font)
+        image.text((self.ability_x, self.ability_y + pushdown), ability,
+                   fill=self.color, font=self.ability_font)
+        image.text((x, self.ability_y + pushdown), ability_value, fill=self.color,
+                   font=self.ability_font)
+        image.text((self.ability_attr_x, self.ability_attr_y + pushdown), ability_attr,
+                   fill=self.color, font=self.attributes_font)
+        image.multiline_text((self.details_x, self.details_y + pushdown), ability_details,
+                             fill=self.color, font=self.details_font,
+                             spacing=self.details_spacing)
+        image.line([self.line_start, self.line_y + pushdown, self.line_end, self.line_y + pushdown],
+                   fill=self.color)
+
+    def add_flavour(self, image: ImageDraw, text: str):
+        box = image.multiline_textbbox((0, 0), text, font=self.flavour_font,
+                                       spacing=self.flavour_spacing)
+        # box[3] = bottom side of text box, equal to height in this case
+        image.multiline_text((self.flavour_x, self.flavour_y - box[3]), text,
+                             fill=self.color, font=self.flavour_font,
+                             spacing=self.flavour_spacing)
 
     def create_images(self):
         with Image.open("Template empty.png") as image:
             new_image = image.copy()
 
         draw = ImageDraw.Draw(new_image)
-        # Single line header looks fine
-        # Cannot measure length of multiline text!
-        self.draw_header(draw, "MUFFIN MAN")
-        self.draw_card_text(draw, "AN ABILITY", "1", "physical technique",
-                            "Does something?\nMaybe not", "muffin", "NORMAL", "5 HP")
-        self.add_flavour(draw, 1, "You thought it was a man")
+        self.draw_long_header(draw, "MUFFIN\nMUNCHER")
+        self.draw_ability(draw, "MUFFIN PUNCH", "physical technique",
+                          "Very spongy\nTastes good", ability_value="5")
+        self.draw_ability(draw, "MUFFIN BODY", "passive", "Absorbs blows",
+                          spacing=2)
+        self.draw_footer(draw, "muffin", "NORMAL", "5 HP")
+        self.add_flavour(draw,
+                         "You thought it was a man\nBut it was a muffin!")
         new_image.show()
         new_image.save("Card images/Muffin man.png")
 
